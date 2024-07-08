d4962ae5b7e04e6b25368cd86dbe33c716407c30
Dale Short <short.dale@gmail.com>
2024-07-07 20:53:48 -0400

    Sync downstairs pc

--- goserial.go
+++ goserial.go
@@ -4,71 +4,120 @@ import (
 	"fmt"
 	"time"
 
+	"github.com/spf13/viper"
 	"go.bug.st/serial"
 )
 
 func main() {
 	println("starting main")
+
+	viper.SetConfigName("config") // name of config file (without extension)
+	viper.SetConfigType("yaml")   // REQUIRED if the config file does not have the extension in the name
+	viper.AddConfigPath(".")      // optionally look for config in the working directory
+	err := viper.ReadInConfig()   // Find and read the config file
+	if err != nil {               // Handle errors reading the config file
+		panic(fmt.Errorf("fatal error config file: %w", err))
+	}
+
+	err = checkAllConfig()
+	if err != nil {
+		panic(err.Error())
+	}
+
 	mode := &serial.Mode{
-		BaudRate: 9600,
+		BaudRate: viper.GetInt("baud"),
 		Parity:   serial.EvenParity,
 		DataBits: 7,
 		StopBits: serial.OneStopBit,
 	}
-	listenerPort := "COM5"
 
-	forwarderPort := "COM8"
+	listenerPort := viper.GetString("listener")
+
+	forwarderPort := viper.GetString("forwarder")
+
+	tportPort := viper.GetString("tport")
 
 	listener, err := serial.Open(listenerPort, mode)
+	listener.SetReadTimeout(time.Millisecond * 10)
 
 	if err != nil {
 		println("error opening port: %v", err)
 	}
-
-	listener.SetReadTimeout(time.Millisecond * 10)
 
 	fowarder, err := serial.Open(forwarderPort, mode)
 	fowarder.SetReadTimeout(time.Millisecond * 10)
 	if err != nil {
 		fmt.Printf("error opening port: %v \n", err)
 	}
+
+	tport, err := serial.Open(tportPort, mode)
+
+	if err != nil {
+		fmt.Printf("error opening port: %v \n", err)
+	}
+
 	go func() {
 		println("running listener loop")
 		for {
-			buff := make([]byte, 100)
+			buff := make([]byte, viper.GetInt("buffer"))
 			n, err := listener.Read(buff)
 			if err != nil {
 				fmt.Printf("error reading listener %v: %v \n", listenerPort, err)
 			}
 			if n != 0 {
 				fowarder.Write(buff[:n])
-				fmt.Printf("writing %v %s, %s \n", n, forwarderPort, string(buff[:n]))
+				fmt.Printf("writing to forwarder (drip feed) %v %s, %s \n", n, forwarderPort, string(buff[:n]))
+				tportCopy := buff[:n]
+				go tport.Write(tportCopy)
 			}
 
 		}
 	}()
 
 	go func() {
-		println("running forw arder loop")
+		println("running forwarder loop")
 
 		for {
-			buff := make([]byte, 100)
+			buff := make([]byte, viper.GetInt("buffer"))
 			n, err := fowarder.Read(buff)
 			if err != nil {
 				fmt.Printf("error reading forwarder %v: %v \n", forwarderPort, err)
 			}
 			if n != 0 {
 				listener.Write(buff[:n])
-				fmt.Printf("writing %v  %s , %s \n", n, listenerPort, string(buff[:n]))
+				fmt.Printf("writing to listener (cnc) %v  %s , %s \n", n, listenerPort, string(buff[:n]))
+				tportCopy := buff[:n]
+				go tport.Write(tportCopy)
 			}
 
 		}
 	}()
 
-	go func() {
-		println("test func")
-	}()
+	print("running forwarder with T port sniffer \n")
 
 	for {
+
 	}
+}
+
+func checkAllConfig() error {
+
+	keys := []string{
+		"baud",
+		"listener",
+		"forwarder",
+		"tport",
+		"buffer",
+	}
+
+	for _, key := range keys {
+
+		isSet := viper.IsSet(key)
+		if !isSet {
+			fmt.Printf("unable to locate key %v", key)
+			return fmt.Errorf("unable to locate key %v", key)
+		}
+	}
+	return nil
+
 }
